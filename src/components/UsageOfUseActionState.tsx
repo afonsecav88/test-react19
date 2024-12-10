a220dbff5f1782bb4f125a461acbf8d5c9e877bf
Adrian Fonseca Vega <afonsecav88@gmail.com>
2024-12-09 22:58:16 -0300

    added isPending at state

--- src/components/UsageOfUseActionState.tsx
+++ src/components/UsageOfUseActionState.tsx
@@ -1,23 +1,27 @@
 import { useActionState } from 'react';
 
 async function increment(previousState: number) {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
   return previousState + 1;
 }
 
 export const UsageOfUseActionState = () => {
   // Usage
   // Using information returned by a form action
-  const [state, formAction] = useActionState(increment, 0);
+  const [state, formAction, isPending] = useActionState(increment, 0);
   console.log('state', state);
   return (
     <form>
       {state}
       <button
         formAction={formAction}
+        disabled={isPending}
         style={{
+          background: isPending ? '#0DC143' : '#FF0000',
           marginLeft: '20px',
           border: 'solid 1px',
-          background: '#ffddee',
         }}>
         Increment
       </button>
