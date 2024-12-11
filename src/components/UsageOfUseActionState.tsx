1cb33eaa632bff7f673ef2f90f2d5a670306b797
Adrian Fonseca Vega <afonsecav88@gmail.com>
2024-12-10 21:58:43 -0300

    update UsageOfUseActionState component

--- src/components/UsageOfUseActionState.tsx
+++ src/components/UsageOfUseActionState.tsx
@@ -1,4 +1,4 @@
-import { useActionState } from 'react';
+import { useActionState, version } from 'react';
 
 async function increment(previousState: number) {
   await new Promise((resolve) => {
@@ -13,20 +13,24 @@ export const UsageOfUseActionState = () => {
   // Using information returned by a form action
   //Call useActionState at the top level of your component to access the return value of an action from the last time a form was submitted.
   const [state, formAction, isPending] = useActionState(increment, 0);
+
   console.log('state', state);
   return (
-    <form>
-      {state}
-      <button
-        formAction={formAction}
-        disabled={isPending}
-        style={{
-          background: isPending ? '#0DC143' : '#FF0000',
-          marginLeft: '20px',
-          border: 'solid 1px',
-        }}>
-        Increment
-      </button>
-    </form>
+    <>
+      <title>{`Hola, React ${version}`}</title>;
+      <form>
+        {state}
+        <button
+          formAction={formAction}
+          disabled={isPending}
+          style={{
+            background: isPending ? '#0DC143' : '#FF0000',
+            marginLeft: '20px',
+            border: 'solid 1px',
+          }}>
+          Increment
+        </button>
+      </form>
+    </>
   );
 };
